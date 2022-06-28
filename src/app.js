fabe5e1eea6b4a85e897d905a486c409ebc1a12a
Xillians <chkxillians@gmail.com>
2022-06-28 10:38:57 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require ('dotenv');
+const app = express();
+dotenv.config();
+const axios = require('axios').default;
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/', async (req, res) => {
+    try {
+        let requestBody = req.body;
+        const targetUrl = requestBody.targetUrl;
+        delete requestBody.targetUrl;
+        const targetResponse = await axios({
+            method: 'post',
+            url: targetUrl,
+            data: requestBody
+        });
+        res.status = targetResponse.status;
+        return res.send(targetResponse.data)
+    } catch (error) {
+        res.status(error.response.status);
+        res.send({
+            "Error": error.message    
+        });
+    }
+});
+app.listen(port, () => {
+    console.log("listening on port: ", port);
+})
